43f1399e5939e7cd6024a587d5d8d7e15a16e1a8
Eric <eric.gro.2000@gmail.com>
2024-04-29 00:42:15 +0200

    Add code

---
+++ Assets/Wp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wp : MonoBehaviour {
+
+	// Use this for initialization
+	public Text uiText;
+	void Start () {
+		//uiText = GetComponentInChildren<Text>();
+		//uiText = GameObject.Find("Text");
+
+        // Access and modify the text value
+        //uiText.Text = "Hello, World!";
+		//Debug.Log(uiText.text = "You Won!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+// -190.374
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.name == "PortalGunFPSController") {
+			uiText.text = "You Won!";
+		}
+	}
+}
