5f885aab8f45fed6fb690b774e548b558a1fa3ec
Eric <eric.gro.2000@gmail.com>
2024-04-29 00:30:18 +0200

    Add code

---
+++ Assets/ProtoPack/Scripts/SlimeMoveTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeMoveTest : MonoBehaviour
+{
+
+	void Update()
+	{
+
+		transform.Translate(Vector3.forward / 60);
+		transform.Rotate(Vector3.up * 1.5f);
+
+	}
+}
